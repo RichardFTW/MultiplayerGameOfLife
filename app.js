22cbb1e0bed5de6dadc08c144bd2845c460c8579
Richard <richard1248c@gmail.com>
2017-03-05 10:02:00 -0500

    yeah
    yeah

--- app.js
+++ app.js
@@ -7,16 +7,25 @@ app.get('/', function(req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-server.listen(2000);
-
-var SOCKET_LIST = {};
+server.listen(3000);
+//var SOCKET_LIST = {};
 
 var io = require('socket.io')(server, {});
-
-io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
-	SOCKET_LIST[socket.id] = socket;
-	socket.on('disconnect', function(){
-		delete SOCKET_LIST[socket.id];
+io.sockets.on('connection', function(socket) {
+	console.log('socket connection');
+//	socket.id = Math.random();
+//	SOCKET_LIST[socket.id] = socket;
+	
+	socket.on('disconnect', function() {
+		console.log('socket disconnection');
+//		delete SOCKET_LIST[socket.id];
 	});
 });
+
+setInterval(function() {
+//	var pack = [];
+//	for(var i in SOCKET_LIST) {
+//		var socket = SOCKET_LIST[i];
+//		socket.emit('newPositions', pack);
+//	}
+}, 1000/25);

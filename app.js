1748386b27f252fbc75df8ee70346a901936d01b
Richard <richard1248c@gmail.com>
2017-03-05 08:52:50 -0500

    more initial stuff
    more initial stuff

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.use('/client', express.static(__dirname + '/client'));
 server.listen(2000);
 
 var SOCKET_LIST = {};
+
+var io = require('socket.io')(server, {});
+
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
+	})
+});
